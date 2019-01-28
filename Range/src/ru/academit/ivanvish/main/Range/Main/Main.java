b323dd3f23fb6d13b75aafd4a49dd83cd664337f
Ivan <vapanda569@mail.ru>
2019-01-28 12:31:03 +0700

    Второй коммит

--- Range/src/ru/academit/ivanvish/main/Range/Main/Main.java
+++ Range/src/ru/academit/ivanvish/main/Range/Main/Main.java
@@ -1,9 +1,20 @@
 package ru.academit.ivanvish.main.Range.Main;
 
+import ru.academit.ivanvish.main.Range.Range.Range;
+
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(3, 13);
-        range.print();
-        range.scanner();
+        Range range = new Range(4, 13);
+        Range range1 = new Range(7, 19);
+
+        Range interSectionResult = range.getIntersection(range1);
+        if (interSectionResult == null) {
+            System.out.println((Range) null);
+        } else {
+            System.out.println("Объединение двух интервалов = " + range.getFrom() + ";" + interSectionResult.getTo());
+            System.out.println("Разность двух интервалов = " + range.getFrom() + ";" + interSectionResult.getFrom());
+            System.out.println("Разность двух интервалов = " + range.getTo() + ";" + interSectionResult.getTo());
+            System.out.println("Область пересечения двух интервалов = " + interSectionResult.getFrom() + ";" + range.getTo());
+        }
     }
 }
--- Range/src/ru/academit/ivanvish/main/Range/Range/Range.java
+++ Range/src/ru/academit/ivanvish/main/Range/Range/Range.java
@@ -1,59 +1,51 @@
 package ru.academit.ivanvish.main.Range.Range;
 
-import java.util.Scanner;
-
 public class Range {
-    private double to;
+
     private double from;
+    private double to;
 
-    private Range(double to, double from) {
+    public Range(double from, double to) {
+        this.from = from;
         this.to = to;
+
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getTo() {
+        return (this.to);
+    }
+
+    public void setFrom(double from) {
         this.from = from;
     }
 
-    private void setTo(double to) {
-        this.to = to;
+    public double getFrom() {
+        return (this.from);
     }
 
-    private double getTo() {
-        return to;
-    }
 
-    private void setFrom(double from) {
-        this.from = from;
-    }
-
-    private double getFrom() {
-        return from;
-    }
-
-    private double getLength() {
-        return (from - to);
+    private double getLength1() {
+        return Math.abs((from - to));
     }
 
     private boolean isInside(double enterPointC) {
-        if (enterPointC >= to && enterPointC <= from) {
+        if ((enterPointC <= to && enterPointC >= from)) {
             return true;
         }
         return false;
     }
 
-    private void print() {
-        System.out.println("Длина отрезка AB: " + getLength());
-        System.out.println("Точка A: " + getTo());
-        System.out.println("Точка B: " + getFrom());
+    public Range getIntersection(Range range) {
+        if (Math.max(from, range.from) >= Math.min(to, range.to)) {
+            return null;
+        } else {
+            new Range(Math.max(from, range.from), Math.min(to, range.to));
+        }
+        return range;
     }
-
-    private void scanner() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите точку C: ");
-        double enterPointC = scanner.nextDouble();
-        System.out.println(isInside(enterPointC) ? "Точка C лежит на отрезке AB" : "Точка C не лежит на отрезке AB");
-    }
-
-   /* public static void main(String[] args) {
-        Range range = new Range(3, 13);
-        range.print();
-        range.scanner();
-    }*/
 }
+
