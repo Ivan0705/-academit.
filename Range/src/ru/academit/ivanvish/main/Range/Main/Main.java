217f250d14549df7adb1ad6e2289bc09a1524505
Ivan <vapanda569@mail.ru>
2019-01-31 20:35:17 +0700

    Второй коммит

--- Range/src/ru/academit/ivanvish/main/Range/Main/Main.java
+++ Range/src/ru/academit/ivanvish/main/Range/Main/Main.java
@@ -8,13 +8,26 @@ public class Main {
         Range range1 = new Range(7, 19);
 
         Range interSectionResult = range.getIntersection(range1);
+        Range[] summing = range.getMerge(range1);
+        Range[] difference = range.getDifference(range1);
+
         if (interSectionResult == null) {
             System.out.println((Range) null);
         } else {
-            System.out.println("Объединение двух интервалов = " + range.getFrom() + ";" + interSectionResult.getTo());
-            System.out.println("Разность двух интервалов = " + range.getFrom() + ";" + interSectionResult.getFrom());
-            System.out.println("Разность двух интервалов = " + range.getTo() + ";" + interSectionResult.getTo());
-            System.out.println("Область пересечения двух интервалов = " + interSectionResult.getFrom() + ";" + range.getTo());
+            System.out.println("Сумма двух интервалов = " + summing[0].getFrom() + " : " + summing[0].getTo());
+            if ((difference[0].getFrom() != difference[0].getTo()) && (difference[1].getFrom() != difference[1].getTo())) {
+                System.out.println("ОбъектА - ОбъектВ = " + difference[0].getFrom() + " : " + difference[0].getTo());
+                System.out.println("ОбъектВ - ОбъектА = " + difference[1].getFrom() + " : " + difference[1].getTo());
+            } else if ((difference[0].getFrom() == difference[0].getTo()) && (difference[1].getTo() == difference[1].getFrom())) {
+                System.out.println("ОбъектА - ОбъектВ = ");
+                System.out.println("ОбъектВ - ОбъектА = ");
+            } else if (difference[0].getFrom() == difference[0].getTo()) {
+                System.out.println("ОбъектА - ОбъектВ = ");
+                System.out.println("ОбъектВ - ОбъектА = " + difference[1].getFrom() + " : " + difference[1].getTo());
+            } else {
+                System.out.println("ОбъектА - ОбъектВ = " + difference[0].getFrom() + " : " + difference[0].getTo());
+                System.out.println("ОбъектВ - ОбъектА = ");
+            }
         }
     }
 }
--- Range/src/ru/academit/ivanvish/main/Range/Range/Range.java
+++ Range/src/ru/academit/ivanvish/main/Range/Range/Range.java
@@ -8,7 +8,6 @@ public class Range {
     public Range(double from, double to) {
         this.from = from;
         this.to = to;
-
     }
 
     public void setTo(double to) {
@@ -27,25 +26,92 @@ public class Range {
         return (this.from);
     }
 
-
     private double getLength1() {
         return Math.abs((from - to));
     }
 
     private boolean isInside(double enterPointC) {
-        if ((enterPointC <= to && enterPointC >= from)) {
-            return true;
-        }
-        return false;
+        return (enterPointC <= to && enterPointC >= from);
     }
 
     public Range getIntersection(Range range) {
-        if (Math.max(from, range.from) >= Math.min(to, range.to)) {
+        if (Math.max(from, range.from) > Math.min(to, range.to)) {
             return null;
         } else {
             new Range(Math.max(from, range.from), Math.min(to, range.to));
         }
         return range;
+    }
+
+    public Range[] getMerge(Range range) {
+     /*   if ((range.from >= from) && (range.to >= to)) {
+            return new Range[]{new Range(from, range.to)};
+        } else if ((from >= range.from) && (to >= range.to)) {
+            return new Range[]{new Range(range.from, to)};
+        } else if ((range.from >= from) && (range.to >= to) && (range.from >= to)) {
+            return new Range[]{new Range(from, to), new Range(range.from, range.to)};
+        } else if ((from >= range.from) && (to >= range.to) && (from >= range.to)) {
+            return new Range[]{new Range(range.from, range.to), new Range(from, to)};
+        } else if ((range.from >= from) && (range.to >= to) && (to == range.from)) {
+            return new Range[]{new Range(from, range.to)};
+        } else if ((from >= range.from) && (to >= range.to) && (range.to == from)) {
+            return new Range[]{new Range(range.from, to)};
+        } else if ((range.from >= from) && (to >= range.to)) {
+            return new Range[]{new Range(from, to)};
+        } else if ((from >= range.from) && (range.to >= to)) {
+            return new Range[]{new Range(range.from, range.to)};//8
+        } else if ((from == range.from) && (range.to == to)) {
+            return new Range[]{new Range(from, to)};
+        } else if ((from == range.from) && (to >= range.to)) {
+            return new Range[]{new Range(range.from, to)};
+        } else if ((from == range.from) && (to <= range.to)) {
+            return new Range[]{new Range(from, range.to)};
+        } else if ((range.from >= from) && (to == range.to)) {
+            return new Range[]{new Range(range.from, to)};
+        } else if ((from >= range.from) && (to == range.to)) {
+            return new Range[]{new Range(from, range.to)};
+        }
+        return new Range[]{};*/
+        if (((Math.max(from, to) > Math.min(range.from, range.to)) || (Math.max(range.from, range.to) > Math.min(from, to)))) {
+            return new Range[]{new Range(Math.min(from, range.from), Math.max(to, range.to))};
+        } else {
+            return null;
+        }
+    }
+
+    public Range[] getDifference(Range range) {
+      /*  if ((range.from >= from) && (range.to >= to)) {
+            return new Range[]{new Range(from, range.from), new Range(to, range.to)};
+        } else if ((from >= range.from) && (to >= range.to)) {
+            return new Range[]{new Range(range.from, from), new Range(range.to, to)};
+        } else if ((range.from >= from) && (range.to >= to) && (range.from >= to)) {
+            return null;
+        } else if ((from >= range.from) && (to >= range.to) && (from >= range.to)) {
+            return null;
+        } else if ((range.from >= from) && (range.to >= to) && (to == range.from)) {
+            return new Range[]{new Range(from, to), new Range(to, range.to)};
+        } else if ((from >= range.from) && (to >= range.to) && (range.to == from)) {
+            return new Range[]{new Range(range.from, range.to), new Range(range.to, to)};
+        } else if ((range.from >= from) && (range.to <= to)) {
+            return new Range[]{new Range(from, range.from), new Range(range.to, to)};
+        } else if ((range.from <= from) && (range.to >= to)) {
+            return new Range[]{new Range(range.from, from), new Range(to, range.to)};
+        } else if ((range.from == from) && (range.to == to)) {
+            return new Range[]{};
+        } else if ((range.from == from) && (to >= range.to)) {
+            return new Range[]{new Range(range.from, range.to), new Range(range.to, to)};
+        } else if ((range.from == from) && (to <= range.to)) {
+            return new Range[]{new Range(range.from, to), new Range(to, range.to)};
+        } else if ((range.from >= from) && (range.to == to)) {
+            return new Range[]{new Range(from, range.from), new Range(range.from, range.to)}
+        } else if ((range.from <= from) && (range.to == to)) {
+            return new Range[]{new Range(range.from, from), new Range(from, range.to)};
+        }
+        return new Range[]{};*/
+        if (((Math.max(from, to) > Math.min(range.from, range.to)) || (Math.max(range.from, range.to) > Math.min(from, to)))) {
+            return new Range[]{new Range(Math.min(range.from, from), Math.max(from, range.from)), new Range(Math.min(to, range.to), Math.max(range.to, to))};
+        }
+        return null;
     }
 }
 
