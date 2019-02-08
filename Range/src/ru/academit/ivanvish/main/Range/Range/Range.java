f60928a241733fc067056855a43e2c9820f2106b
Ivan <vapanda569@mail.ru>
2019-02-08 14:15:23 +0700

    Range

--- Range/src/ru/academit/ivanvish/main/Range/Range/Range.java
+++ Range/src/ru/academit/ivanvish/main/Range/Range/Range.java
@@ -38,31 +38,60 @@ public class Range {
     }
 
     public Range getIntersection(Range range) {
-        if (Math.max(from, range.from) >= Math.min(to, range.to)) {
+        double from = Math.max(this.from, range.from);
+        double to = Math.min(this.to, range.to);
+        if (from >= to) {
             return null;
+        } else {
+            return new Range(from, to);
         }
-        return range;
     }
 
     public Range[] getMerge(Range range) {
-        Range unionTwoIntervals = new Range(Math.min(from, range.from), Math.max(to, range.to));
-        Range object1 = new Range(Math.min(from, range.from), Math.min(to, range.to));
-        Range object2 = new Range(Math.max(from, range.from), Math.max(to, range.to));
+        Range object1 = new Range(from, to);
+        Range object2 = new Range(range.from, range.to);
 
-        if (((Math.max(from, to) > Math.min(range.from, range.to)) || (Math.max(range.from, range.to) > Math.min(from, to)))) {
-            return new Range[]{unionTwoIntervals};
+        if (Math.max(from, to) > Math.min(range.from, range.to)) {
+            if ((from <= range.from) && (to <= range.to)) {
+                return new Range[]{new Range(from, range.to)};
+            } else if ((from >= range.from) && (to >= range.to)) {
+                return new Range[]{new Range(range.from, to)};
+            } else if ((from <= range.from) && (to >= range.to)) {
+                return new Range[]{new Range(from, to)};
+            } else {
+                return new Range[]{new Range(range.from, range.to)};
+            }
         } else {
-            return new Range[]{object1, object2};
+            if ((from <= range.from) && (to <= range.to) && (range.from >= to)) {
+                return new Range[]{object1, object2};
+            } else {
+                return new Range[]{object2, object1};
+            }
         }
+
+      /*  Range object1 = new Range(Math.min(from, range.from), Math.min(to, range.to));
+        Range object2 = new Range(Math.max(from, range.from), Math.max(to, range.to));
+        Range unionTwoObjects = new Range(Math.min(from, range.from), Math.max(to, range.to));
+
+        if (Math.max(from, range.from) >= Math.min(range.to, to)) {
+            return new Range[]{object1, object2};
+        } else {
+            return new Range[]{unionTwoObjects};
+        }*/
     }
 
     public Range[] getDifference(Range range) {
-        if (range.from > from) {
+        if ((from < range.to) || (range.from > to)) {
+            return new Range[]{(new Range(from, to))};
+        } else if ((range.from > from) && (to > range.to)) {
+            return new Range[]{new Range(from, range.from), new Range(range.to, to)};
+        } else if ((from >= range.from) && (to >= range.to)) {
+            return new Range[]{};
+        } else if ((from >= range.from) && (to > range.to)) {
+            return new Range[]{new Range(range.to, to)};
+        } else {
             return new Range[]{new Range(from, range.from)};
-        } else if (range.from < from) {
-            return new Range[]{new Range(range.from, from)};
         }
-        return new Range[]{};
     }
 }
 
